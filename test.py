48975c96a264edd125117bbdbc72d43b71a4f8df
brihad24 <brihad1692@gmail.com>
2022-11-19 22:05:32 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,41 +2,43 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import time
 
 path = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(path)
-driver.get("https://learn.upes.ac.in/?new_loc=%2Fultra%2Fcourse")
+driver = webdriver.Chrome(executable_path=path)
+
+driver.get("https://learn.upes.ac.in/ultra/calendar")
 print(driver.title)
 
-username = "500082592"
-password = "##########"
+# Logs in to the account
+def login():
+    username = "500082592"
+    password = "brihad1692"
 
-# Agrees to cookies
-driver.find_element("id", "agree_button").click()
+    # Agrees to cookies
+    driver.find_element("id", "agree_button").click()
 
-# Inputs username and password
-uname = driver.find_element("id", "user_id") 
-uname.send_keys(username)
+    # Inputs username and password
+    uname = driver.find_element("id", "user_id") 
+    uname.send_keys(username)
 
-pword = driver.find_element("id", "password") 
-pword.send_keys(password)
+    pword = driver.find_element("id", "password") 
+    pword.send_keys(password)
 
-# Submits the entries
-driver.find_element("id", "entry-login").click()
+    # Submits the entries
+    driver.find_element("id", "entry-login").click()
 
-# Wait for login process to complete. 
-WebDriverWait(driver=driver, timeout=10).until(lambda x: x.execute_script("return document.readyState === 'complete'"))
+login()
 
-# Verify that the login was successful.
-error_message = "Incorrect username or password."
-
-# Retrieve any errors found. 
-errors = driver.find_elements(By.CLASS_NAME, "flash-error")
-
-# When errors are found, the login will fail. 
-if any(error_message in e.text for e in errors): 
-    print("[!] Login failed")
-else:
-    print("[+] Login successful")
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "bb-calendar1-month")))
+time.sleep(2)
+driver.find_element("id", "bb-calendar1-month").click()
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "bb-calendar1-deadline")))
+time.sleep(2)
+driver.find_element("id", "bb-calendar1-deadline").click()
+time.sleep(10)
 
 driver.quit()
